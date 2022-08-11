734d0b4162bebea584880427d1ae5445e2743081
fruityloops1 <fruityloops@littun.co>
2022-08-11 18:16:02 +0200

    update exlaunch

--- src/lib/util/math/bitset.hpp
+++ src/lib/util/math/bitset.hpp
@@ -1,45 +1,48 @@
 #pragma once
 
-#include "common.hpp"
+#include <common.hpp>
 #include <concepts>
 
-namespace exl::asml {
-
-    template <std::integral Underlying, Underlying Low, Underlying High = Low + 1>
+namespace exl::util {
+    
+    template<
+        std::integral Underlying, 
+        Underlying _Low,
+        Underlying _High = _Low + 1>
     struct Mask {
-        static constexpr Underlying LowBit = Low;
-        static constexpr Underlying HighBit = High;
-        static constexpr Underlying Count = High - Low;
+        static constexpr Underlying Low = _Low;
+        static constexpr Underlying High = _High;
+        static constexpr Underlying Count = _High - _Low;
         static constexpr Underlying Value() {
             auto base = (1 << Count) - 1;
             return base << Low;
         }
     };
 
-    template <std::integral Underlying>
+    template<std::integral Underlying>
     class BitSet {
-      private:
+        private:
         Underlying m_Data;
 
-      public:
+        public:
         constexpr inline BitSet() : m_Data() {}
         constexpr inline BitSet(Underlying data) : m_Data(data) {}
 
-        template <Mask Mask>
+        template<Mask Mask>
         constexpr Underlying inline BitsOf() const {
             /* Take out the bits we want. */
             auto value = m_Data & Mask.Value();
             /* Shift down the bits. */
-            return value >> Mask.LowBit;
+            return value >> Mask.Low;
         }
 
-        template <Mask Mask>
+        template<Mask Mask>
         constexpr void inline SetBits(Underlying value) {
             /* Carve out the bits not in the mask. */
             m_Data &= ~Mask.Value();
 
             /* Prepare value to be written. */
-            auto v = value << Mask.LowBit;
+            auto v = value << Mask.Low;
             v &= Mask.Value();
 
             /* OR in the bits. */
@@ -47,21 +50,18 @@ namespace exl::asml {
         }
 
         /* Wrappers to construct masks. */
-        template <Underlying Low, Underlying High>
+        template<Underlying Low, Underlying High>
         constexpr Underlying inline BitsOf() const {
             return BitsOf<Mask<Underlying, Low, High>>();
         }
-        template <Underlying Low, Underlying High>
+        template<Underlying Low, Underlying High>
         constexpr void inline SetBits(Underlying value) {
             SetBits<Mask<Underlying, Low, High>>(value);
         }
-
+        
         /* Conversion operators. */
-        constexpr inline Underlying& operator=(const Underlying& value) {
-            m_Data = value;
-            return this;
-        }
+        constexpr inline Underlying& operator=(const Underlying& value) { m_Data = value; return this; }
 
         constexpr inline Underlying Value() const { return m_Data; }
     };
-}; // namespace exl::asml
+};
--- src/lib/util/sys/mem_layout.hpp
+++ src/lib/util/sys/mem_layout.hpp
@@ -23,44 +23,48 @@ namespace exl::util {
     namespace mem_layout {
         static constexpr int s_MaxModules = 13;
         inline int s_ModuleCount = -1;
-    } // namespace mem_layout
+    }
 
     namespace impl {
         void InitMemLayout();
 
         namespace mem_layout {
-            inline ModuleInfo s_ModuleInfos[util::mem_layout::s_MaxModules];
+            inline std::array<ModuleInfo, util::mem_layout::s_MaxModules> s_ModuleInfos;
         }
-    } // namespace impl
+    }
 
     static inline const ModuleInfo& GetModuleInfo(int index) {
-        EXL_ASSERT(index < mem_layout::s_ModuleCount);
-        return impl::mem_layout::s_ModuleInfos[index];
+        EXL_ASSERT(index < mem_layout::s_ModuleCount); 
+        return impl::mem_layout::s_ModuleInfos.at(index); 
     }
 
     namespace mem_layout {
 
-#if defined EXL_AS_KIP
-/* TODO */
-#error "Not implemented..."
-#elif defined EXL_AS_RTLD
+    #if defined EXL_AS_KIP
+        /* TODO */
+        #error "Not implemented..."
+    #elif defined EXL_AS_RTLD
         static constexpr int s_RtldModuleIdx = 0;
         static constexpr int s_MainModuleIdx = 1;
         static constexpr int s_SelfModuleIdx = s_RtldModuleIdx;
-    }
+
+        }
 
     static inline const ModuleInfo& GetRtldModuleInfo() { return GetModuleInfo(mem_layout::s_RtldModuleIdx); }
     static inline const ModuleInfo& GetMainModuleInfo() { return GetModuleInfo(mem_layout::s_MainModuleIdx); }
-#elif defined EXL_AS_MODULE
+    #elif defined EXL_AS_MODULE
         static constexpr int s_RtldModuleIdx = 0;
         static constexpr int s_MainModuleIdx = 1;
 
         /* Decided at runtime. */
         inline int s_SelfModuleIdx = -1;
-    }
+
+        }
     static inline const ModuleInfo& GetRtldModuleInfo() { return GetModuleInfo(mem_layout::s_RtldModuleIdx); }
     static inline const ModuleInfo& GetMainModuleInfo() { return GetModuleInfo(mem_layout::s_MainModuleIdx); }
-#endif
+    #endif
 
-        static inline const ModuleInfo& GetSelfModuleInfo() { return GetModuleInfo(mem_layout::s_SelfModuleIdx); }
-    }; // namespace exl::util
+    
+    static inline const ModuleInfo& GetSelfModuleInfo() { return GetModuleInfo(mem_layout::s_SelfModuleIdx); }
+    static inline const ModuleInfo& GetSdkModuleInfo() { return GetModuleInfo(mem_layout::s_ModuleCount - 1); }
+};
